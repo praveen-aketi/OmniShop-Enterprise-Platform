94c7434251f9604564323a109332a57f1d10585e
praveen-aketi <prvn.aketi@gmail.com>
2025-11-28 00:29:17 +0530

    Update EKS version to 1.30

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -37,7 +37,7 @@ module "eks" {
   version         = "~> 19.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.27"
+  cluster_version = "1.30"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
