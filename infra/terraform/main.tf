551a99bdc125194f184acd988585efa67597056c
praveen-aketi <prvn.aketi@gmail.com>
2025-11-28 00:48:00 +0530

    Configure Kubernetes provider for EKS

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,12 +5,27 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.0, < 5.0.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
   }
 }
 
 provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
 data "aws_availability_zones" "available" {}
