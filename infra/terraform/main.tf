697b3aeec23b3dc33f9a6bee14f3173a541d10df
praveen-aketi <prvn.aketi@gmail.com>
2025-11-28 01:05:22 +0530

    Enable EKS public endpoint access

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -53,6 +53,7 @@ module "eks" {
 
   cluster_name    = var.cluster_name
   cluster_version = "1.30"
+  cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
