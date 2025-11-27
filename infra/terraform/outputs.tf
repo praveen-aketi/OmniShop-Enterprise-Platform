1a528d1bf24ff4b1970babd2504ca5c298c7c579
praveen-aketi <prvn.aketi@gmail.com>
2025-11-28 00:21:06 +0530

    Add missing cluster_name output

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -10,3 +10,8 @@ output "cluster_endpoint" {
 output "cluster_ca" {
   value = module.eks.cluster_certificate_authority_data
 }
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_id
+}
