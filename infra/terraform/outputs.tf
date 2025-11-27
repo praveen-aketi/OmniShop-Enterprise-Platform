18ea65f91a5d579652602812503fa4e980e0269e
praveen-aketi <prvn.aketi@gmail.com>
2025-11-28 00:54:43 +0530

    Remove kubeconfig_command output

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,7 +1,4 @@
-output "kubeconfig_command" {
-  description = "Command to obtain kubeconfig for the cluster"
-  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_id}"
-}
+
 
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
